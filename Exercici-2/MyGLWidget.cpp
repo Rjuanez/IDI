ce9883d4cb3ac46cc05f1005640fdeb08f6f29d5
Ruben <ruben@netvn-1.home>
2022-04-21 01:08:28 +0200

    a

--- Exercici-2/MyGLWidget.cpp
+++ Exercici-2/MyGLWidget.cpp
@@ -105,7 +105,7 @@ void MyGLWidget::patrTransform ()
   glm::mat4 TG(1.0f);
   TG = glm::translate(TG, posPorter);
   //    TG = glm::scale(TG, glm::vec3(1/escalaPatr, 1/escalaPatr, 1/escalaPatr));
-    TG = glm::rotate(TG, 90.f, glm::vec3(0, 1, 0));
+    TG = glm::rotate(TG, -90.0f, glm::vec3(0, 1, 0));
   TG = glm::translate(TG, -centreBasePatr);
   glUniformMatrix4fv(transLoc, 1, GL_FALSE, &TG[0][0]);
 }
