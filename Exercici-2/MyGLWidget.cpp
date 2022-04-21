94c53305b598e893383f1548f052dbdcda6368e1
Ruben <ruben@10-192-17-180client.eduroam.upc.edu>
2022-04-21 12:40:12 +0200

    a

--- Exercici-2/MyGLWidget.cpp
+++ Exercici-2/MyGLWidget.cpp
@@ -155,8 +155,8 @@ void MyGLWidget::mouseMoveEvent(QMouseEvent *e)
 {
   makeCurrent();
     
-    factorAngleX += float(e->x() - xClick)/20.0;
-    factorAngleY += float(e->y() - yClick)/20.0;
+    factorAngleX += float(e->x() - xClick)/100.0;
+    factorAngleY += float(e->y() - yClick)/100.0;
 
   xClick = e->x();
   yClick = e->y();
